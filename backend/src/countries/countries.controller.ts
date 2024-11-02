4394b5ada139a5e6d04a4b00da517ed84c37c74f
Laura Segouras <lausegouras@gmail.com>
2024-11-02 12:23:41 -0300

    feat countries controller

---
+++ backend/src/countries/countries.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CountriesService } from './countries.service';
+
+@Controller('countries')
+export class CountriesController {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private readonly countriesService: CountriesService) {}
+
+  @Get('available')
+  getAvailableCountries() {
+    return this.countriesService.getAvailableCountries();
+  }
+
+  @Get(':countryCode/info')
+  getCountryInfo(@Param('countryCode') countryCode: string) {
+    return this.countriesService.getCountryInfo(countryCode);
+  }
+}
